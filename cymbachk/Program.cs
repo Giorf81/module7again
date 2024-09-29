b045cdbfae93dbd9c28b8553024671a25d3067a9
Giorf81 <rejroberts7@gmail.com>
2024-09-29 15:45:12 +0300

    Добавьте файлы проекта.

---
+++ cymbachk/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.InteropServices;
+
+namespace cymbachk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        class Rectangle()
+        {
+            int a, b;
+
+            public int Square()
+            {
+
+                return a * b;
+            }
+
+            public Rectangle(int first, int second)
+            {
+                a = first;
+                b = second;
+            }
+            public Rectangle(int side)
+            {
+                a = side;
+            }
+
+            public Rectangle()
+            {
+                a = 6; b = 4;
+            }
+        }
+    }
+
+}
